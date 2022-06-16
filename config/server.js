8b1dc4f631210620c50449177bd8b465edd6e3e9
Brayan Steven Urbina Gomez <burbina@estratec.local>
2022-06-15 19:43:45 -0500

    Update server

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '04226bce16fa7cbf49137af0a9cfce04'),
-    },
+  proxy: true,
+  url: env('MY_HEROKU_URL'),
+  app: { 
+    keys: env.array('APP_KEYS')
   },
-});
+})
